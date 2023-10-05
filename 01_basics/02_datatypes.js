351f7880498de663cfa4f85456a9a85be9519132
Amanbh997 <144616372+Amanbh997@users.noreply.github.com>
2023-10-05 13:42:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3 + 3); 
+
+// Code readability should be high
+
+console.log("Amam");
+
+let name = "Aman"  // string
+let age  = 26  //number
+let isloggedIn = false //boolean
+let state;
+
+// number => 2 to power 53
+//bigint (if the number is too huge)
+// string -> ""
+// boolean (True or false)
+// null (it is a datatype and a stand alone value as well) (null is a representation of an empty value)
+// undefined => when value is not assigned 
+// symbol = unique
+
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
